ac22961e4c5ddf747a5445a1ba8598b911ca5372
Jean Fabre <contact@fabrejean.net>
2016-08-22 17:01:29 +0300

    - Fixed Actions - Cleaned up actions

--- Assets/PlayMaker Custom Actions/Level/MoveGameObjectToScene.cs
+++ Assets/PlayMaker Custom Actions/Level/MoveGameObjectToScene.cs
@@ -10,88 +10,69 @@ namespace HutongGames.PlayMaker.Actions
 {
 	[ActionCategory("SceneManager")]
 	[Tooltip("Move a GameObject from its current scene to a new scene. It is required that the GameObject is at the root of its current scene.")]
-	public class MoveGameObjectToScene : FsmStateAction
+	public class MoveGameObjectToScene : GetSceneActionBase
 	{
 		[RequiredField]
 		[Tooltip("The Root GameObject to move to the referenced scene")]
 		public FsmOwnerDefault gameObject;
 
-		[Tooltip("The reference options of the Scene")]
-		public GetSceneActionBase.SceneSimpleReferenceOptions sceneReference;
-
-		[Tooltip("The name of the scene to load. The given sceneName can either be the last part of the path, without .unity extension or the full path still without the .unity extension")]
-		public FsmString sceneByName;
-
-		[Tooltip("The index of the scene to load.")]
-		public FsmInt sceneAtIndex;
+		[RequiredField]
+		[Tooltip("Only root GameObject can be moved, set to true to get the root of the gameobject if necessary, else watch for failure events.")]
+		public FsmBool findRootIfNecessary;
 
 		[ActionSection("Result")]
-
-		[Tooltip("True if SceneReference resolves to a scene")]
+		[Tooltip("True if the merge succeeded")]
 		[UIHint(UIHint.Variable)]
-		public FsmBool sceneFound;
+		public FsmBool success;
 
-		[Tooltip("Event sent if SceneReference resolves to a scene")]
-		public FsmEvent sceneFoundEvent;
+		[Tooltip("Event sent if merge succeeded")]
+		public FsmEvent successEvent;
 
-		[Tooltip("Event sent if SceneReference do not resolve to a scene")]
-		public FsmEvent sceneNotFoundEvent;
+		[Tooltip("Event sent if merge failed. Check log for information")]
+		public FsmEvent failureEvent;
 
-		Scene _scene;
-		bool _sceneFound;
+		GameObject _go;
 
 		public override void Reset()
 		{
+			base.Reset ();
 			gameObject = null;
-
-			sceneReference = GetSceneActionBase.SceneSimpleReferenceOptions.SceneAtIndex;
-			sceneByName = null;
-			sceneAtIndex = null;
-
+			findRootIfNecessary = null;
+			success = null;
+			successEvent = null;
+			failureEvent = null;
 		}
 
 		public override void OnEnter()
 		{
-			GetScene ();
+			base.OnEnter ();
 
 			if (_sceneFound) {
 
-				SceneManager.MoveGameObjectToScene(Fsm.GetOwnerDefaultTarget (gameObject), _scene);
+				_go = Fsm.GetOwnerDefaultTarget (gameObject);
 
-				Fsm.Event(sceneFoundEvent);
+				if (findRootIfNecessary.Value) {
+					_go = _go.transform.root.gameObject;
+				}
+
+				if (_go.transform.parent == null) {
+					SceneManager.MoveGameObjectToScene(_go, _scene);
+					success.Value = true;
+					Fsm.Event (successEvent);
+
+				}else{
+					
+					LogError("GameObject must be a root ");
+					success.Value = false;
+					Fsm.Event(failureEvent);
+				}
+
+				Fsm.Event(foundEvent);
+
+				_go = null;
 			}
 
 			Finish();
-		}
-
-		void GetScene()
-		{
-			try{
-				switch (sceneReference) {
-				case GetSceneActionBase.SceneSimpleReferenceOptions.SceneAtIndex:
-					_scene = SceneManager.GetSceneAt (sceneAtIndex.Value);	
-					break;
-				case GetSceneActionBase.SceneSimpleReferenceOptions.SceneByName:
-					_scene = SceneManager.GetSceneByName (sceneByName.Value);
-					break;
-				}
-			}catch(Exception e) {
-				LogError (e.Message);
-			}
-
-			if (_scene == new Scene()) {
-				_sceneFound = false;
-				if (!sceneFound.IsNone) {
-					sceneFound.Value = false;
-				}
-				Fsm.Event(sceneNotFoundEvent);
-			} else {
-				_sceneFound = true;
-				if (!sceneFound.IsNone) {
-					sceneFound.Value = true;
-				}
-
-			}
 		}
 	}
 }
